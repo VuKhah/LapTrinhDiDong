2c183cb455b78a0ef3a3596a556ceefbf8f0cfa6
VuKhah <TrVuKhah27@gmail.com>
2025-03-02 17:25:40 +0700

    Bài tập 1: viết giao diện login , register

---
+++ Ex_W4/LoginRegisterApp/app/src/main/java/com/example/loginregisterapp/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.loginregisterapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView tvRegister = findViewById(R.id.tvRegister);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
